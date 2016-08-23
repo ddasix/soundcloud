23519d90f771f49b353fa5f6572d6507061c195b
ddasix <ddasix55@gmail.com>
2016-08-23 21:36:05 +0900

    d

--- sound/app/Http/Controllers/Auth/AuthController.php
+++ sound/app/Http/Controllers/Auth/AuthController.php
@@ -99,6 +99,7 @@ class AuthController extends Controller
 
         try {
             SocialAuth::login('soundcloud',function($user, $details) {
+                dd($details);
                 $user->id = $details->id;
                 $user->nickname = $details->nickname;
                 $user->name = $details->full_name;
