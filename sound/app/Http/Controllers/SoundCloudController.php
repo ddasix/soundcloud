24b4bf861ee1b05e39a3a7183ed9f5f0014032b3
ddasix <ddasix55@gmail.com>
2016-08-23 22:22:20 +0900

    f

--- sound/app/Http/Controllers/SoundCloudController.php
+++ sound/app/Http/Controllers/SoundCloudController.php
@@ -23,7 +23,7 @@ class SoundCloudController extends Controller
         $user = OauthIdentities::find($authuser->id);
 
         $client = new Client;
-        dd(config('soundcloud.client_id'));
+        dd(config('eloquent-oauth.soundcloud.client_id'));
 
         $r = $client->get("http://api.soundcloud.com/playlists/".$user->provider_user_id."?client_id=".config('soundcloud.client_id'));
         dd($r);
